9abd246af1b80a6cc0a1f95e4b7a23ce7ba68178
jjaranda13 <jjaranda13@alcatel-lucent.com>
2018-07-19 13:17:51 +0200

    20180719_JJ

--- splitter/iterator.py
+++ splitter/iterator.py
@@ -1,7 +1,7 @@
 #iterates over the functions matrix
 import collapser
 
-def iterate(con,matrix,num_deployable_units):
+def iterate_fake(con,matrix,num_deployable_units):
 	matrix = []
 	matrix[0] = ['Matrix', 'file_main.main', ['dir1.file1.f1', 'dir1.file1.f2'], 'dir1.file2.f3', 'dir2.file3.f4', 'dir2.file3.f5']
 	matrix[1] = ['file_main.main', 0, 0, 0, 0, 0]
@@ -11,3 +11,23 @@ def iterate(con,matrix,num_deployable_units):
 	matrix[5] = ['dir2.file3.f5',0,0,0,0,0]
 
 	return matrix
+
+
+def iterate(con,matrix,desired_num_du)
+	''' this function receives the matrix as input and the number of desired DUs.
+		The desired number of DUs will be (normally) the number of available machines.
+		Therefore the final number of DUs must be equal or bigger than number of desired DUs.
+		Without any iteration, the number of DU is the number of functions.
+		The matrix will be colapsed iteratively, reducing the number of DUs, by colapsing the 
+		pair of functions which invoke each other more times.
+		when the number of DU is equal to number of desired DUs, then the process stops'''
+
+
+		# for testing
+		return iterate_fake(con,matrix,desired_num_du)
+		#iteration 0
+		num_du= len(matrix[0])-1
+
+
+
+
